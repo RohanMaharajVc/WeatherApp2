fc018e1b9a49add6ba5ed9920c722397822a6f21
RohanMaharajVc <romaharaj@iie.ac.za>
2026-05-12 08:45:28 +0200

    Splash screen
    Created the splash screen frontend, and completed the code for the buttons in the backend

--- app/src/main/java/com/rohan/weatherapp/MainActivity.kt
+++ app/src/main/java/com/rohan/weatherapp/MainActivity.kt
@@ -1,16 +1,41 @@
 package com.rohan.weatherapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //declarations
+    private lateinit var btnStart: Button
+    private lateinit var btnExit: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //typecasting
+        btnStart = findViewById(R.id.btnStart)
+        btnExit = findViewById(R.id.btnExit)
+
+
+        btnStart.setOnClickListener {
+          val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        btnExit.setOnClickListener {
+            finishAffinity()
+
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

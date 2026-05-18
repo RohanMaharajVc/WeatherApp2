2dea63bceadfc9074e0f52399cf05636f843a2ae
RohanMaharajVc <romaharaj@iie.ac.za>
2026-05-18 08:31:06 +0200

    Data class
    Created a data class to capture the weather information

---
+++ app/src/main/java/com/rohan/weatherapp/WeatherDay.kt
@@ -0,0 +1,21 @@
+package com.rohan.weatherapp
+
+data class WeatherDay(
+
+    //Store the name of the day (e.g Monday, Tuesday - Sunday)
+    val dayName: String,
+
+    //Store minimum tempreture
+    val minTempreture: Int = 0,
+
+    //Store our maximum temp
+    var maxTempreture: Int = 0,
+
+    //Store our weather Condition (e.g sunny, rainy)
+    var weatherCondition: String = "",
+
+    //Tracks whether the user has entered data for this day
+    var dataCaptured: Boolean = false
+
+
+)
